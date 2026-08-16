07da88f2162324905563cd377646906742819a60
Aditya Singh <adirajawat19@gmail.com>
2026-08-16 14:48:22 +0530

    Time: 4 ms (23.23%) | Memory: 9.4 MB (30.80%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
   bool isIsomorphic(string s, string t) {
-    int m1[256] = {0}, m2[256] = {0}; 
-          
-          // Get the length of the strings
-          int n = s.size(); 
-          
-          // Traverse each character of the strings
-          for (int i = 0; i < n; ++i) {
-              // If previous positions of current characters differ, return false
-              if (m1[s[i]] != m2[t[i]]) return false;
-              
-              // Update the position with current index + 1
-              m1[s[i]] = i + 1;
-              m2[t[i]] = i + 1;
-          }
-          
-          // If no mismatch is found, return true
-          return true;
+    if (s.length() != t.length()) return false;
+
+    unordered_map<char, char> map_s_to_t;
+    unordered_map<char, char> map_t_to_s;
+
+    for (int i = 0; i < s.length(); ++i) {
+        char char_s = s[i];
+        char char_t = t[i];
+
+        // Check if the mapping from s to t exists and is consistent
+        if (map_s_to_t.count(char_s)) {
+            if (map_s_to_t[char_s] != char_t) return false;
+        } else {
+            map_s_to_t[char_s] = char_t;
+        }
+
+        // Check if the mapping from t to s exists and is consistent
+        if (map_t_to_s.count(char_t)) {
+            if (map_t_to_s[char_t] != char_s) return false;
+        } else {
+            map_t_to_s[char_t] = char_s;
+        }
+    }
+
+    return true;
 }
 };
