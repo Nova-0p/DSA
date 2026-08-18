f619187d428bea99d10ccdb2c0a2713e29f7f746
Aditya Singh <adirajawat19@gmail.com>
2026-08-18 18:12:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (93.42%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
 
+    double helper (double x,long long n,double ans){
+            //base case
+            if (n==0){
+                return ans;
+            }
+          //odd
+            if(n%2!=0){
+                return helper(x,n-1,ans*x);
+            }
+
+            //even , dont put if odd already checked
+           
+                return helper(x*x,n/2,ans);
+            
+
+    }
+
     double myPow(double x, int n) {
 
         // to avoid overflow make long long copy of n
@@ -10,6 +27,7 @@ public:
             x=1/x;
             nn=-nn;
         }
+        /*
         while(nn>0){
             //odd
             if(nn%2!=0){
@@ -22,7 +40,8 @@ public:
                 x=x*x;
                 nn=nn/2;
             }
-        }
-        return ans;
+        }*/
+        
+        return helper(x,nn,ans);
     }
 };
