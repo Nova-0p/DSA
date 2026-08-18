3cb6f5aaa37fc9ff052e235d36d27b0bda989c95
Aditya Singh <adirajawat19@gmail.com>
2026-08-18 16:23:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (5.11%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+
+    long long helper(int n,string s,int i,long long ans,int sign){
+        if(i==n || s[i] < '0' || s[i] > '9'){
+            //base case
+            return ans;
+        }
+
+        int digit=s[i]-'0';
+        ans=ans*10+digit;
+       
+    
+    if (sign * ans <= INT_MIN) return INT_MIN;
+    if (sign * ans >= INT_MAX) return INT_MAX;
+
+             return helper(n,s,i+1,ans,sign);
+}
     int myAtoi(string s) {
         int i=0;
         int sign=1;
@@ -13,8 +29,11 @@ public:
             i++;
         }else if(i<n && s[i]=='+'){
             i++;
-        }
-       while (i < s.size() && isdigit(s[i])) {
+        } // sign handling
+
+        ///  string to digit also recursively using helper func
+
+       /* while (i < s.size() && isdigit(s[i])) {
          ans = ans * 10 + (s[i] - '0');
          i++;
           if (sign == 1 && ans > INT_MAX) {
@@ -25,6 +44,13 @@ public:
                 return INT_MIN;
             }
        }
-       return ans*sign;
+       return ans*sign; */
+    ans=helper(n,s,i,0,sign);
+
+  
+ 
+ return ans*sign;
+
+
     }
 };
