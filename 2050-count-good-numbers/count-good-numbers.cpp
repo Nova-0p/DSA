bfbc5cdd54088d8dfbb2754695f480098dc37e69
Aditya Singh <adirajawat19@gmail.com>
2026-08-19 14:18:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (71.72%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     const long long MOD = 1e9 + 7;
+    long long helper (long long x,long long n,long long ans){
+            //base case
+            if (n==0){
+                return ans%MOD;
+            }
+            if(n%2!=0){
+                return helper(x,n-1,(ans*x)%MOD);
+            }
+            //even , dont put if odd already checked
+                return helper((x*x)%MOD,n/2,(ans)%MOD);
 
-    long long po(long long x, long long n) {
-        
-        // Base case
-        if (n == 0) {
-            return 1;
-        }
-
-        long long half = po(x, n / 2);
-
-        if (n % 2 == 0) {
-            return (half * half) % MOD;
-        }
-
-        return (x * half % MOD * half) % MOD;
     }
-
     int countGoodNumbers(long long n) {
+        long long evenPos=(n+1)/2;
+        long long oddPos=(n/2);
         
-        long long evenPos = (n + 1) / 2;
-        long long oddPos = n / 2;
 
-        long long ans1 = po(5, evenPos);
-        long long ans2 = po(4, oddPos);
+        long long ans1=helper(5,evenPos,1);
+        long long ans2=helper(4,oddPos,1);
 
-        return (ans1 * ans2) % MOD;
+        return (ans1*ans2)%MOD;
     }
 };
