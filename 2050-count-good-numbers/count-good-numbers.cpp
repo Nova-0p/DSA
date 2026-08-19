4a5dc21fda96b2bbd4c9b3c41339eb5b327f751e
Aditya Singh <adirajawat19@gmail.com>
2026-08-19 14:23:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (97.49%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -14,12 +14,10 @@ public:
 
     }
     int countGoodNumbers(long long n) {
-        long long evenPos=(n+1)/2;
-        long long oddPos=(n/2);
-        
+        long long ans1 = helper(5, (n + 1) / 2, 1);
+long long ans2 = helper(4, n / 2, 1);
 
-        long long ans1=helper(5,evenPos,1);
-        long long ans2=helper(4,oddPos,1);
+return (ans1 * ans2) % MOD;
 
         return (ans1*ans2)%MOD;
     }
