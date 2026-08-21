5f784b24cf3ce9d536570786045209c3e53508c8
Aditya Singh <adirajawat19@gmail.com>
2026-08-21 16:56:42 +0530

    Time: 73 ms (56.52%) | Memory: 193.3 MB (74.92%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        const int MOD = 1e9 + 7;
+
+        vector<int> pse(n);
+        vector<int> nse(n);
+
+        stack<int> st;
+
+        // Previous Smaller Element
+        for (int i = 0; i < n; i++) {
+
+            while (!st.empty() && arr[st.top()] >= arr[i]) {
+                st.pop();
+            }
+
+            if (st.empty()) {
+                pse[i] = -1;
+            }
+            else {
+                pse[i] = st.top();
+            }
+
+            st.push(i);
+        }
+
+        // Clear stack
+        while (!st.empty()) {
+            st.pop();
+        }
+
+        // Next Smaller or Equal Element
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.empty() && arr[st.top()] > arr[i]) {
+                st.pop();
+            }
+
+            if (st.empty()) {
+                nse[i] = n;
+            }
+            else {
+                nse[i] = st.top();
+            }
+
+            st.push(i);
+        }
+
+        long long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            long long left = i - pse[i];
+            long long right = nse[i] - i;
+
+            long long subarrays = left * right;
+
+            long long contribution = subarrays * arr[i];
+
+            ans = (ans + contribution) % MOD;
+        }
+
+        return ans;
+    }
+};
