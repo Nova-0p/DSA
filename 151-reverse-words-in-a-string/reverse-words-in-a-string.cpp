b4eeb3af4ea1840d51829b85cd3ac42af52632be
Aditya Singh <adirajawat19@gmail.com>
2026-09-07 20:48:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (69.70%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -4,26 +4,23 @@ public:
         int n = s.size();
         int i = n - 1;
         string result = "";
-
         while (i >= 0) {
 
-            while (i >= 0 && s[i] == ' ') {
+            while (i >= 0 && s[i] == ' ') {    // this is to remove spaces if there are at end
                 i--;
-            }
-
-            if (i < 0) {
+            }                     
+            if (i < 0) {  // check if we reached beginning
                 break;
             }
-            int j = i;
-
-            while (i >= 0 && s[i] != ' ') {
+            int j = i;  // end of current word;
+            
+            while (i >= 0 && s[i] != ' ') {  // start of current word (moving left until we find space)
                 i--;
             }
+            string word = s.substr(i + 1, j - i);  // create substring from i+1 to j-1;
 
-            string word = s.substr(i + 1, j - i);
-
-            if (!result.empty()) {
-                result += " ";
+            if (!result.empty()) {   // add space but only if there if there is already a word 
+                result += " ";       // so that we dont add spacw before first word
             }
 
             result += word;
